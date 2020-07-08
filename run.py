c7dcfcf07f09c77ae6bc8e799539d4a925efb55f
sudoRicheek <richeekdas2001@gmail.com>
2020-07-08 21:49:50 +0530

    optimize tf

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from AppWindowQt import Ui_MainWindow
+import tensorflow as tf
+import os
+
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 main_ui = Ui_MainWindow()
 main_ui.execute()
